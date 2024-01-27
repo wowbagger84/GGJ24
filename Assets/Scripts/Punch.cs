fa183a9a9385b48cce2c56d6dd60ab90e49b7bb5
wowbagger84 <wowbagger84@gmail.com>
2024-01-27 13:25:32 +0100

    restored broken stuff

---
+++ Assets/Scripts/Punch.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punch : MonoBehaviour
+{
+	public GameObject punchPrefab;
+	public GameObject jokeLinePrefab;
+	public Transform punchSpawn;
+
+	public string[] joke;
+	public int jokeLineIndex = 0;
+
+	//TODO: read/parse the text from the punchline system, scriptable object?
+	//TODO: cycle jokes, so all have been used before repeating
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		joke = new string[] { "What do you call a fish with no eyes?", "A fsh!" };
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			//TODO: spawn the next joke line or punchline
+
+			if (jokeLineIndex < joke.Length - 1)
+			{
+				var newLine = Instantiate(jokeLinePrefab, punchSpawn.position, transform.rotation);
+				newLine.GetComponent<Bubble>().jokeLine = true;
+				newLine.GetComponent<Bubble>().text = joke[jokeLineIndex];
+				jokeLineIndex++;
+			}
+			else
+			{
+				var newLine = Instantiate(punchPrefab, punchSpawn.position, transform.rotation);
+				newLine.GetComponent<Bubble>().text = joke[jokeLineIndex];
+				jokeLineIndex = 0;
+			}
+		}
+	}
+}
