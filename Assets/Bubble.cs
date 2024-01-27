5edc7052e1b6268c40bfb0b518ca9a95379c8733
wowbagger84 <wowbagger84@gmail.com>
2024-01-27 11:33:47 +0100

    bubble tweek

--- Assets/Bubble.cs
+++ Assets/Bubble.cs
@@ -1,29 +1,40 @@
 using DG.Tweening;
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bubble : MonoBehaviour
 {
+	public float time = 0.4f;
+
+	float myFloat = 0;
+	BoxCollider2D myCollider;
+
 	// Start is called before the first frame update
 	void Start()
 	{
-		transform.localScale = new Vector3(0, 1, 1);
-		transform.DOScaleX(5, 0.15f).SetEase(Ease.OutBack).OnComplete(Cleanup);
-		//GetComponentInChildren<TextMeshProUGUI>().DOText("PUNCHLINE!", 0.3f, true, ScrambleMode.All);
+		DOTween.To(() => myFloat, x => myFloat = x, 5, time).OnUpdate(UpdateCollider);
+
+		myCollider = GetComponent<BoxCollider2D>();
+
+		//TODO: adjust time according to the length of the text
+
+		//TODO: Get the text from the punchline system.
+		GetComponentInChildren<TextMeshProUGUI>().DOText("PUNCHLINE!", time);
+
+		//TODO: Approximate the size of the text and set the size of the bubble accordingly
+		GetComponentInChildren<Image>().GetComponent<RectTransform>().DOSizeDelta(new Vector2(300, 30), time).SetEase(Ease.OutBack).OnComplete(Cleanup);
 	}
 
+	private void UpdateCollider()
+	{
+		myCollider.offset = new Vector2(myFloat / 2, 0.2f);
+		myCollider.size = new Vector2(myFloat, 0.5f);
+	}
 
 	void Cleanup()
 	{
-		Destroy(gameObject, 1);
+		Destroy(gameObject, 2);
 	}
-	//private void Update()
-	//{
-	//	if (Input.GetButtonDown("Fire1"))
-	//	{
-	//		Start();
-	//	}
-	//}
 }
