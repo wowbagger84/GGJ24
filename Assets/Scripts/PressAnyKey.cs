3414a123a718ebe22b466b6eebad19dc33cdc499
wowbagger84 <wowbagger84@gmail.com>
2024-01-27 10:03:19 +0100

    added art. worked on scene flow, removed some fonts

--- Assets/Scripts/PressAnyKey.cs
+++ Assets/Scripts/PressAnyKey.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class PressAnyKey : MonoBehaviour
 {
+	public string SceneToLoad = "Controls";
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -15,7 +16,7 @@ public class PressAnyKey : MonoBehaviour
 	{
 		if (Input.anyKeyDown)
 		{
-			UnityEngine.SceneManagement.SceneManager.LoadScene("Controls");
+			UnityEngine.SceneManagement.SceneManager.LoadScene(SceneToLoad);
 		}
 	}
 
