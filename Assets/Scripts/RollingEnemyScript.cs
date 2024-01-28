c415ceace14f14d9733383f204febed0002cab32
wowbagger84 <wowbagger84@gmail.com>
2024-01-28 10:20:18 +0100

    created prefabs for level objects

--- Assets/Scripts/RollingEnemyScript.cs
+++ Assets/Scripts/RollingEnemyScript.cs
@@ -1,41 +1,48 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
 public class EnemyScript : MonoBehaviour
 {
-    public float rollSpeed = 50f;
-    public float moveSpeed = 2f;
-    private Rigidbody2D rb;
-    public GameObject deathEffect;
-    ParticleSystem deathSplosion;
-    Animator animator;
+	public float rollSpeed = 50f;
+	public float moveSpeed = 2f;
+	private Rigidbody2D rb;
+	public GameObject deathEffect;
+	ParticleSystem deathSplosion;
+	Animator animator;
+	Vector3 startPos;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		startPos = transform.position;
+		deathSplosion = GetComponentInChildren<ParticleSystem>();
+	}
 
-        deathSplosion = GetComponentInChildren<ParticleSystem>();
-    }
+	void Update()
+	{
+		rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+		rb.angularVelocity = rollSpeed;
 
-    void Update()
-    {
-        rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
-        rb.angularVelocity = rollSpeed;
-    }
+		if (transform.position.y < -30)
+		{
+			rb.velocity = Vector2.zero;
+			transform.position = startPos;
+		}
+	}
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Bubble"))
-        {
-            var newEffect = Instantiate(deathEffect, transform.position, Quaternion.identity);
-            
-            transform.DOScale(0, 0.3f).SetEase(Ease.InSine).OnComplete(() => Destroy(gameObject));
-            
-            deathSplosion.Play();
-            animator.SetTrigger("Death");
-            Destroy(newEffect, 1);
-        }
-    }
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Bubble"))
+		{
+			var newEffect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+
+			transform.DOScale(0, 0.3f).SetEase(Ease.InSine).OnComplete(() => Destroy(gameObject));
+
+			deathSplosion.Play();
+			animator.SetTrigger("Death");
+			Destroy(newEffect, 1);
+		}
+	}
 }
