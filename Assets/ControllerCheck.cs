27cb34639d76e071b02f16ffafc6d41a2f637503
wowbagger84 <wowbagger84@gmail.com>
2024-01-28 14:36:52 +0100

    level 2 fix

--- Assets/ControllerCheck.cs
+++ Assets/ControllerCheck.cs
@@ -7,10 +7,43 @@ public class ControllerCheck : MonoBehaviour
 	public GameObject keyboardControls;
 	public GameObject controllerControls;
 
+	private bool connected = false;
+
+	IEnumerator CheckForControllers()
+	{
+		while (true)
+		{
+			var controllers = Input.GetJoystickNames();
+
+			if (!connected && controllers.Length > 0)
+			{
+				connected = true;
+				Debug.Log("Connected");
+				keyboardControls.SetActive(false);
+				controllerControls.SetActive(true);
+
+			}
+			else if (connected && controllers.Length == 0)
+			{
+				connected = false;
+				Debug.Log("Disconnected");
+				keyboardControls.SetActive(true);
+				controllerControls.SetActive(false);
+			}
+
+			yield return new WaitForSeconds(1f);
+		}
+	}
+
+	void Awake()
+	{
+		StartCoroutine(CheckForControllers());
+	}
+
 	// Start is called before the first frame update
 	void Start()
 	{
-		string[] joystickNames = Input.GetJoystickNames();
+		//string[] joystickNames = Input.GetJoystickNames();
 		//Debug.Log("Joysticks found: " + joystickNames.Length);
 		//for (int i = 0; i < joystickNames.Length; i++)
 		//{
@@ -22,12 +55,12 @@ public class ControllerCheck : MonoBehaviour
 		//	}
 		//}
 
-		if (joystickNames.Length > 0)
-		{
-			//Debug.Log("Gamepad found: " + joystickNames[0]);
-			keyboardControls.SetActive(false);
-			controllerControls.SetActive(true);
-		}
+		//if (joystickNames.Length > 0)
+		//{
+		//	//Debug.Log("Gamepad found: " + joystickNames[0]);
+		//	keyboardControls.SetActive(false);
+		//	controllerControls.SetActive(true);
+		//}
 
 	}
 
